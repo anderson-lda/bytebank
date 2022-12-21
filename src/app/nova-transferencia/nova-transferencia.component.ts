2620e071fb52c584f9ca0ca9ed555b0094aa89c2
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-12-21 11:57:47 -0300

    with styles and defined the variables

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,5 +6,12 @@ import { Component } from "@angular/core";
     styleUrls:['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent{
+    valor: number;
+    destino: number;
 
+    transferir(){
+        console.log('SOlicitado nova transferencia');
+        console.log('destino: '+this.destino);
+        console.log('valor: '+this.valor);
+    }
 }
