bffd3e2f97059cc908a418c3bdee53b266548475
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-12-21 18:01:45 -0300

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencias: any[] = [];
+
+  constructor(private service: TransferenciaService){
+    
+  }
 
   transferir($event){
-    const transferencia = {...$event, data: new Date()}; //apenas os objetos internos de evento são passados para transferencia
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
   }
 }
